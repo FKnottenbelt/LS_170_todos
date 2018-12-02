12ab7cc7a8b910d0c9ed84ddcac33cb2a02508c9
Femke Knottenbelt <femknotgithub@gmail.com>
2018-12-02 16:14:48 +0000

    Add user message: The list has been created

--- todo.rb
+++ todo.rb
@@ -15,17 +15,21 @@ get "/" do
   redirect "/lists"
 end
 
+# View all the lists
 get "/lists" do
   @lists = session[:lists]
   erb :lists, layout: :layout
 end
 
+# Create a new list
 post "/lists" do
   @new_list = params[:list_name]
   session[:lists] << { name: @new_list, todos: [] }
+  session[:success] = "The list has been created"
   redirect "/lists"
 end
 
+# Render the new list form
 get "/lists/new" do
   erb :new_list, layout: :layout
 end
