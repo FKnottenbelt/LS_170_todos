5a67b42f467ca7bb70b46b53f8d63a99116127e4
Femke Knottenbelt <femknotgithub@gmail.com>
2018-12-02 12:08:39 +0000

    Add add new list option

--- todo.rb
+++ todo.rb
@@ -19,3 +19,13 @@ get "/lists" do
   @lists = session[:lists]
   erb :lists, layout: :layout
 end
+
+post "/lists" do
+  @new_list = params[:list_name]
+  session[:lists] << { name: @new_list, todos: [] }
+  redirect "/lists"
+end
+
+get "/lists/new" do
+  erb :new_list, layout: :layout
+end
