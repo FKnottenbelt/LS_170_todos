5d4bf336b4db1ba23e5c05e406db70eeb6088034
Femke Knottenbelt <femknotgithub@gmail.com>
2018-12-03 21:09:05 +0000

    Add minitest assertions to capybara

--- test/todo_test.rb
+++ test/todo_test.rb
@@ -10,31 +10,33 @@ Capybara.app = Sinatra::Application
 
 class TodoTest < Minitest::Test
   include Capybara::DSL
+  include Capybara::Minitest::Assertions
 
   def test_homepage_redirects_to_lists
     visit '/'
-    assert page.has_content?("Todo Tracker")
+    assert_current_path("/lists")
+    assert_content("Todo Tracker")
   end
 
   def test_make_new_valid_todolist
     visit '/lists'
     click_link("New List")
-    assert page.has_content?("Enter the name for your new list")
+    assert_content("Enter the name for your new list")
 
     fill_in 'list_name', with: 'Test List'
     click_button("Save")
-    assert page.has_content?("Test List")
-    assert page.has_content?("The list has been created")
+    assert_content("Test List")
+    assert_content("The list has been created")
   end
 
   def test_new_empty_todolist_gives_error
     visit '/lists'
     click_link("New List")
-    assert page.has_content?("Enter the name for your new list")
+    assert_content("Enter the name for your new list")
 
     fill_in 'list_name', with: ' '
     click_button("Save")
-    assert page.has_content?("List name must by between 1 and 100 characters")
+    assert_content("List name must by between 1 and 100 characters")
   end
 
 end
