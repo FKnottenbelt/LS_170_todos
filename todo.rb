d251cc43534ddfc468a9e71a90a5f3f94946e2e1
Femke Knottenbelt <femknotgithub@gmail.com>
2018-12-14 08:06:54 +0000

    Add helpers for counting todos

--- todo.rb
+++ todo.rb
@@ -9,19 +9,23 @@ configure do
 end
 
 helpers do
-  def count_completed_todos(list)
-    list[:todos].select { |todo| todo[:completed] == true }.count
+  def count_remaining_todos(list)
+    list[:todos].select { |todo| todo[:completed] == false }.count
+  end
+
+  def todos_count(list)
+    list[:todos].count
   end
 
   def list_complete?(list)
-    list[:todos].map {|todo| todo[:completed] }.all? == true &&
-    list[:todos].count > 0
+    count_remaining_todos(list) == 0 && todos_count(list) > 0
   end
 
   def list_class(list)
     list_complete?(list) ? 'complete' : 'uncomplete'
   end
 end
+
 
 before do
   session[:lists] ||= []
