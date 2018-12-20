78a9449ababf3f21e7d48e40af596c69ec2989cb
Femke Knottenbelt <femknotgithub@gmail.com>
2018-12-20 10:24:25 +0000

    Add dialog when deleting items

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,13 @@
-console.log("this is a test");
+$(function(){
+
+  $('form.delete').submit(function(event){
+    event.preventDefault();
+    event.stopPropagation();
+    var ok = confirm('Are you sure you want to delete this item?');
+
+    if (ok) {
+      this.submit();
+    }
+
+  });
+});
